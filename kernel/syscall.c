564ab9b306982a3e22ad16f188d6ea67af6c619a
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2023-08-19 16:31:19 -0300

    implement trace

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -101,6 +101,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_trace(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -126,6 +127,14 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_trace]   sys_trace,
+};
+
+char* syscall_pretty[] = {
+  "fork", "exit", "wait", "pipe", "read", "kill",
+  "exec", "fstat", "chdir", "dup", "getpid", "sbrk",
+  "sleep", "uptime", "open", "write", "mknod", "unlink",
+  "link", "mkdir", "close", "trace"
 };
 
 void
@@ -138,7 +147,11 @@ syscall(void)
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
-    p->trapframe->a0 = syscalls[num]();
+    uint64 ret = syscalls[num]();
+    if (p->trace & 1 << num) {
+      printf("%d: syscall %s -> %d\n", p->pid, syscall_pretty[num - 1], ret);
+    }
+    p->trapframe->a0 = ret;
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
